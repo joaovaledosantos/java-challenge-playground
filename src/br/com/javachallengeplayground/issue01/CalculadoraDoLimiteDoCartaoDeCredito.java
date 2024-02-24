67e7b0a7dab05b65cf1272cbcd9694baa9dd29b6
João V. Santos <joaovaledosantos@gmail.com>
2024-02-24 19:58:35 -0300

    [Issue-1] Calculadora do limite do cartão de crédito

---
+++ src/br/com/javachallengeplayground/issue01/CalculadoraDoLimiteDoCartaoDeCredito.java
@@ -0,0 +1,13 @@
+package br.com.javachallengeplayground.issue01;
+
+public class CalculadoraDoLimiteDoCartaoDeCredito {
+    public static void main(String[] args) {
+        double salario = 500;
+        if (salario >= 5000) {
+            System.out.println("Meu limite de cartão de credito e de 10.000 reais");
+        } else {
+            System.out.println("meu limite de cartão de credito será de 3.000 reais");
+        }
+
+    }
+}
