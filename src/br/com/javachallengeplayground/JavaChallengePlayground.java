e3c3ed22ef13b483adcd9b87c5f790e82fdea91a
João V. Santos <joaovaledosantos@gmail.com>
2024-02-24 18:51:55 -0300

    Inicio projeto javachallengeplayground

---
+++ src/br/com/javachallengeplayground/JavaChallengePlayground.java
@@ -0,0 +1,7 @@
+package br.com.javachallengeplayground;
+
+public class JavaChallengePlayground {
+    public static void main(String[] args) {
+        System.out.println("JavaChallengePlayground");
+    }
+}
