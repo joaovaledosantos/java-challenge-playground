4a0a1b6864f9d139533fa055c1fa0c30de9116aa
João V. Santos <joaovaledosantos@gmail.com>
2024-02-25 06:43:58 -0300

    [issue-07] Qual a cor da camisa da seleção brasileira

--- src/br/com/javachallengeplayground/issue07/QualACorDaCamisaDaSelecaoBrasileira.java
+++ src/br/com/javachallengeplayground/issue07/QualACorDaCamisaDaSelecaoBrasileira.java
@@ -1,4 +1,18 @@
 package br.com.javachallengeplayground.issue07;
 
+import java.util.Scanner;
+
 public class QualACorDaCamisaDaSelecaoBrasileira {
+    public static void main(String[] args) {
+        System.out.println("Qual a cor da camisa da selecao brasileira?");
+        Scanner leitor = new Scanner(System.in);
+        String resposta = leitor.nextLine();
+        System.out.println("Voce respondeu: " + resposta);
+
+        if (resposta.equals("amarelo") || resposta.equals("azul")){
+            System.out.println("Parabens, voce acertou! ");
+        }else {
+            System.out.println("voce errou! ");
+        }
+    }
 }
