1ba235b3e22934b3f9b97954ffce9cc567b8a3fd
João V. Santos <joaovaledosantos@gmail.com>
2024-02-25 06:39:48 -0300

    [issue-07] O Brasil ja venceu quantas copas do mundo

--- src/br/com/javachallengeplayground/issue06/OBrasilJaVenceuQuantasCopasDoMundo.java
+++ src/br/com/javachallengeplayground/issue06/OBrasilJaVenceuQuantasCopasDoMundo.java
@@ -1,4 +1,17 @@
 package br.com.javachallengeplayground.issue06;
 
-public class OBrasilJaVenceuQuantasCopaDoMundo {
+import java.util.Scanner;
+
+public class OBrasilJaVenceuQuantasCopasDoMundo {
+    public static void main(String[] args) {
+        System.out.print("O Brasil ja venceu quantas copas do mundo?");
+        Scanner leitor = new Scanner(System.in);
+        String resposta = leitor.nextLine();
+        System.out.println("Voce respondeu: " + resposta);
+        if (resposta.equals("5")){
+            System.out.println("Parabens, voce acertou! ");
+        }else {
+            System.out.println("Voce errou :( ! ");
+        }
+    }
 }
